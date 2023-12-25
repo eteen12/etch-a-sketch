1ecb30c4c797aee7b3183d1fcf345f2ef6867423
Ethan Breitkreutz <eteensmash@gmail.com>
2023-12-25 15:45:16 -0800

    started to wrap up etch a sketch before large changes.

--- java.js
+++ java.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded",function(){
-    createBoard("4");
-    console.log('Hi')
+    createBoard("16");
+  
+    let btn_popup=document.querySelector("#popup");
+    btn_popup.addEventListener('click',function(){
+        let size = getSize();
+        createBoard(size);
+    })
 })
 
 function createBoard(size){
@@ -13,7 +18,26 @@ function createBoard(size){
 
     for(let i=0;i<numDivs;i++){
         let div=document.createElement('div');
-        div.style.backgroundColor='yellow';
+
+        div.addEventListener('mouseover', function(){
+            div.style.backgroundColor="Black";
+        })
+
         board.insertAdjacentElement("beforeend",div);
+    }
+}
+
+function getSize(){
+    let input = prompt("How large would you like the board?");
+    let message=document.querySelector('#message');
+    if(input==""){
+        message.innerHTML="Please provide a number!";
+    }
+    else if(input<0 || input>100){
+        message.innerHTML="Please provide a number that is greater than 0 but less than 100";
+    }
+    else{
+        message.innerHTML="Thank you!";
+        return input;
     }
 }
