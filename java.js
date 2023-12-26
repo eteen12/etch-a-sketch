27560f1c1eec009d6753dc7b522850226af148d0
Ethan Breitkreutz <eteensmash@gmail.com>
2023-12-25 16:35:23 -0800

    changed when you are allowed to draw by mouse click

--- java.js
+++ java.js
@@ -1,7 +1,21 @@
 let color = "black";
+let click="false";
 
 document.addEventListener("DOMContentLoaded",function(){
     createBoard(16);
+
+    this.documentElement.querySelector("body").addEventListener('click',function(e){
+        if (e.target.tagName != "BUTTON"){
+            click =!click;
+            let draw =document.querySelector("#draw");
+            if(click){
+                draw.innerHTML="Now you can draw";
+            }
+            else{
+                draw.innerHTML="You cannot draw";
+            }
+        }
+    })
   
     let btn_popup=document.querySelector("#popup");
     btn_popup.addEventListener('click',function(){
@@ -43,11 +57,13 @@ function getSize(){
 }
 
 function colorDiv(){
+    if(click){
 if(color =="random"){
      this.style.backgroundColor= `hsl(${Math.random() * 360}, 100%, 50%)`
 }
 else{
     this.style.backgroundColor ='black'
+}
 }
 }
 
